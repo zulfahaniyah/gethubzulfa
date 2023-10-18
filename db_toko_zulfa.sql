5b0fa9f1af031907091dd99d8a8e97fa1dea301b
zulfahaniyah <147356224+zulfahaniyah@users.noreply.github.com>
2023-10-18 12:58:09 +0700

    Add files via upload

---
+++ db_toko_zulfa.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2023 at 07:52 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_toko_zulfa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_toko`
+--
+
+CREATE TABLE `tb_toko` (
+  `idBarang` int(11) NOT NULL,
+  `kodeBarang` varchar(15) NOT NULL,
+  `namaBarang` varchar(200) NOT NULL,
+  `satuanBarang` varchar(15) NOT NULL,
+  `stokBarang` int(11) NOT NULL,
+  `hargaBarang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  ADD PRIMARY KEY (`idBarang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_toko`
+--
+ALTER TABLE `tb_toko`
+  MODIFY `idBarang` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
